68015e43b6d994f2cfa7a20f01c9bcff67f000a8
MuriithiEric <56841703+MuriithiEric@users.noreply.github.com>
2024-10-31 03:44:23 +0300

    pushed changes to the js file

--- js/countdown-custom.js
+++ js/countdown-custom.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function() {
         $(function () {
-            $('#defaultCountdown').countdown({until: new Date(2024, 6, 20, 8)}); // year, month, date, hour
+            $('#defaultCountdown').countdown({until: new Date(2024, 11, 8, 8)}); // year, month, date, hour
         });
 });		
 
